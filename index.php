5c193131ae7a2c6a69313ed215e0af4ae54cad90
Willian Silveira Espindola <87204387+Willespindola@users.noreply.github.com>
2023-02-26 01:47:48 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Document</title>
+    
+</head>
+
+<body>
+    <form autocomplete="off" action="/action_page.php">
+        <div class="autocomplete" style="width:300px;">
+            <input id="myInput" type="text" name="myCountry" placeholder="Country">
+        </div>
+        <input type="submit">
+    </form>
+
+</body>
+<script type="text/javascript" src="js/buscaListaCidades.js"></script>
+
+</html>
